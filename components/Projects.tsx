2eab8f8bd44cc5827abf218135672a673f39dd39
Sachin Jayadev <sachinjayadev18@gmail.com>
2024-04-01 12:23:26 +0530

    Projects section done

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,13 +1,23 @@
 "use client"
+import useUserData from '@/hooks/useUserData';
 import React, { useState } from 'react';
 
 interface Project {
-  id: number;
-  name: string;
+  _id: string;
+  title: string;
   description: string;
+  liveurl: string;
+  githuburl: string;
+  image: {
+    public_id: string;
+    url: string;
+  };
+  techStack: string[];
 }
 
 const Projects = () => {
+  const { userData }= useUserData();
+  const projects: Project[] = userData ? userData.projects : []
   const [showModal, setShowModal] = useState(false);
   const [currentProject, setCurrentProject] = useState<Project | null>(null);
 
@@ -16,36 +26,49 @@ const Projects = () => {
     setShowModal(!showModal);
   };
 
-  const projects: Project[] = [
-    { id: 1, name: 'Project 1', description: 'Description of Project 1' },
-    { id: 2, name: 'Project 2', description: 'Description of Project 2' },
-    { id: 3, name: 'Project 3', description: 'Description of Project 3' },
-  ];
-
   return (
     <section className="text-center mt-12">
-      <h2 className="text-2xl font-bold text-gray-900 mb-4">Projects</h2>
-      {/* Render each project with its own button */}
-      {projects.map((project) => (
-        <div key={project.id} className="mb-4">
-          <p>{project.name}</p>
-          <button
-            onClick={() => toggleModal(project)}
-            className="bg-blue-500 text-white font-bold py-2 px-4 rounded-full mt-2 hover:bg-blue-600"
-          >
-            View Project
-          </button>
+      <h2 className="text-7xl font-bold text-blue-700 mb-4">PROJECTS</h2>
+      <div className='grid grid-cols-2 gap-4'>
+  {projects.map((project: Project) => (
+    <div key={project._id} className="m-4 border-gray-100 shadow-2xl relative group">
+      <div className="relative">
+        <img className='transition-opacity duration-300 group-hover:opacity-50' src={project.image.url} alt="" />
+        <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-between p-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+          <div>
+            <h1 className='font-bold text-2xl mb-2'>{project.title}</h1>
+            <p className="text-sm">{project.techStack.join(', ')}</p>
+          </div>
+          <div className="flex justify-between z-10">
+            <button
+              onClick={() => toggleModal(project)}
+              className="bg-blue-500 text-white font-bold py-2 px-4 rounded-full hover:bg-blue-600"
+            >
+              View Project
+            </button>
+            <div className="flex space-x-2">
+              <a href={project.githuburl} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700">GitHub</a>
+              <a href={project.liveurl} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700">Live</a>
+            </div>
+          </div>
         </div>
-      ))}
-      {/* Modal for each project */}
+        <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-300"></div>
+      </div>
+    </div>
+  ))}
+</div>
+
+
+
+
       {showModal && (
         <div className="fixed inset-0 flex items-center justify-center z-50">
           <div className="absolute inset-0 bg-gray-900 opacity-75" onClick={() => setShowModal(false)}></div>
           <div className="bg-white p-8 rounded-lg z-50">
-            {/* Detailed overview of current project */}
+            
             {currentProject && (
               <>
-                <h3 className="text-xl font-bold text-gray-900 mb-2">{currentProject.name}</h3>
+                <h3 className="text-xl font-bold text-gray-900 mb-2">{currentProject.title}</h3>
                 <p className="text-gray-700">{currentProject.description}</p>
               </>
             )}
--- components/Services.tsx
+++ components/Services.tsx
@@ -7,15 +7,17 @@ const Services = () => {
   const services = userData ? userData.services : [];
   return (
     <section id='services' className="text-center mt-12">
-      <h2 className="text-2xl font-bold text-gray-900 mb-4">Services</h2>
+      <h2 className="text-7xl font-black text-blue-800 mb-4">SERVICES</h2>
       <div className="grid grid-cols-3 gap-4">
         {services.map((service: any) => (
           <div key={service._id} className='p-4 border border-gray-300 rounded-md shadow-md'>
-              <img src={service.image.url} alt="" />
-              <h1>{service.name}</h1>
-              <p>{service.desc}</p>
-              <h2>{service.charge}</h2>
-          </div>
+            <img className='h-3/4' src={service.image.url} alt="" />
+            <div className='flex justify-between items-center mb-2'>
+              <h1 className='text-2xl text-left font-bold text-blue-700'>{service.name}</h1>
+              <h2 className='font-bold text-right bg-blue-700 text-white p-2 m-2 rounded-lg'>{service.charge}</h2>
+            </div>
+            <p className='text-left'>{service.desc}</p>
+           </div>
         ))}
       </div>
     </section>
